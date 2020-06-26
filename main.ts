6641d34afd12087684d85793fa7e0679d33e701e
MINT Genie <66748747+MINTGenie@users.noreply.github.com>
2020-06-26 20:28:24 +0200

    include rainbow on button B press

--- main.ts
+++ main.ts
@@ -1,22 +1,46 @@
-let fade = 0
-let pixels = neopixel.create(DigitalPin.P0, 32, NeoPixelMode.RGB)
-pixels.range(0, 32).showColor(neopixel.colors(NeoPixelColors.White))
-pixels.setBrightness(50)
-let white = 255
-basic.forever(function () {
-    pixels.showColor(neopixel.rgb(white, white, 255))
-    if (fade == 0) {
-        white += -8
-        if (white == 47) {
-            fade = 1
-            basic.pause(400)
-        }
+input.onButtonPressed(Button.B, function () {
+    if (test) {
+        pixels.setBrightness(15)
+        pixels.showRainbow(1, 360)
+        pixels.show()
+        test = 0
     } else {
-        white += 8
-        if (white == 255) {
-            fade = 0
-            basic.pause(400)
-        }
+        pixels.clear()
+        pixels.range(0, 32).showColor(neopixel.colors(NeoPixelColors.White))
+        pixels.setBrightness(50)
+        pixels.showColor(neopixel.rgb(white, white, 255))
+        white = 255
+        fade = 0
+        test = 1
     }
-    basic.pause(100)
+})
+let fade = 0
+let white = 0
+let test = 0
+let pixels: neopixel.Strip = null
+pixels = neopixel.create(DigitalPin.P0, 32, NeoPixelMode.RGB)
+test = 0
+pixels.setBrightness(15)
+pixels.showRainbow(1, 360)
+basic.forever(function () {
+    if (test) {
+        pixels.showColor(neopixel.rgb(white, white, 255))
+        if (fade == 0) {
+            white += -8
+            if (white < 47) {
+                fade = 1
+                basic.pause(400)
+            }
+        } else {
+            white += 8
+            if (white >= 200) {
+                fade = 0
+                basic.pause(400)
+            }
+        }
+        basic.pause(100)
+    } else {
+        pixels.rotate(1)
+        pixels.show()
+    }
 })
